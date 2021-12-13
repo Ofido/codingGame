eb93f0b4f337b087ecde106cc39e76383dab2456
Ofido <danieldsb98@gmail.com>
2021-12-13 10:10:27 -0300

    iniciando jogo

---
+++ nine-mens-morris/nine-mens-morris.py
@@ -0,0 +1,54 @@
+import sys
+import math
+
+# Pare esse jogo temos duas possiveis soluções, fazer baseado no tabuleiro onde por si só ele já escolheria uma melhor opção
+# a segunda é tratar a ordenação da escolha dos melhor comando possivel referente a cada estágio.
+# inicialmente vou tratar os comandos por não ter que lidar muito com a IA, e após isso melhorarei o codigo baseado no tabuleiro.
+
+
+def ordenaComandos(command):
+    command.sort()
+
+player_id = int(input())  # playerId (0,1)
+fields = int(input())  # number of fields
+neighbors = []
+for i in range(fields):
+    neighbors.append(input())  # neighbors of a field (ex: A1:A4;D1)
+
+# game loop
+while True:
+    op_move = input()  # The last move executed from the opponent
+    board = input()  # Current Board and state(0:Player0 | 1:Player1 | 2:Empty) in format field:state and separated by ;
+    nbr = int(input())  # Number of valid moves proposed.
+    command = []
+    for i in range(nbr):
+        # TODO tratar a entrada para os tipos diferentes de jogadas possiveis
+        command.append(input())  # An executable command line
+
+    ordenaComandos(command)
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    print("player_id", file=sys.stderr, flush=True)
+    print(player_id, file=sys.stderr, flush=True)
+    print(type(player_id), file=sys.stderr, flush=True)
+    print("fields", file=sys.stderr, flush=True)
+    print(fields, file=sys.stderr, flush=True)
+    print(type(fields), file=sys.stderr, flush=True)
+    print("neighbors", file=sys.stderr, flush=True)
+    print(neighbors, file=sys.stderr, flush=True)
+    print(type(neighbors), file=sys.stderr, flush=True)
+    print("op_move", file=sys.stderr, flush=True)
+    print(op_move, file=sys.stderr, flush=True)
+    print(type(op_move), file=sys.stderr, flush=True)
+    print("board", file=sys.stderr, flush=True)
+    print(board, file=sys.stderr, flush=True)
+    print(type(board), file=sys.stderr, flush=True)
+    print("nbr", file=sys.stderr, flush=True)
+    print(nbr, file=sys.stderr, flush=True)
+    print(type(nbr), file=sys.stderr, flush=True)
+    print("command", file=sys.stderr, flush=True)
+    print(command, file=sys.stderr, flush=True)
+    print(type(command), file=sys.stderr, flush=True)
+
+    print(command[0])
